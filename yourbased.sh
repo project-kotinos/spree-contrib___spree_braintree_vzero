45781e5b2def82fabb4bfe3f955de3806b87c65e
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-14 20:48:29 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
+export CI=true
+export TRAVIS=true
+export CONTINUOUS_INTEGRATION=true
+export USER=travis
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export RAILS_ENV=test
+export RACK_ENV=test
+export MERB_ENV=test
+export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
+
 apt-get update && apt-get install -y tzdata mysql-server-5.6 mysql-client-core-5.6 mysql-client-5.6
 gem install bundler -v 2.0.1
 # install
